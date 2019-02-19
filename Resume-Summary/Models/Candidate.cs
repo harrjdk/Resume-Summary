0cac0a3fe1c54391cd9b244017104c76470a0410
jdh <jharrison902@gmail.com>
2019-02-19 00:43:10 -0500

    Added resources for Candidates

---
+++ Resume-Summary/Models/Candidate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Resume_Summary.Models
+{
+
+    public class CandidateContext : DbContext
+    {
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Properties.Resources.CandidateDataSource);
+        }
+    }
+
+    public class Candidate
+    {
+        public int CandidateId { get; set; }
+        public ICollection<Resume> Resumes { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CurrentRole { get; set; }
+        public int YearsExperience { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Resume-Summary/Models/Resume.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Resume_Summary.Models
+{
+    public class ResumeContext : DbContext
+    {
+
+        public DbSet<Resume> Resumes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Properties.Resources.ResumesDataSource);
+        }
+    }
+   
+
+    public class Resume
+    {
+        public int ResumeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ResumeText { get; set; }
+        public byte[] ResumeRaw { get; set; }
+        public DateTime LastModTime { get; set; }
+    }
+}
